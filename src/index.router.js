13d3f52b60f7a2259e9e3103d842946a7bf3ce32
Mohammad-marwan <mohammad672001@gmail.com>
2025-11-20 13:39:38 +0300

    first commit

--- src/index.router.js
+++ src/index.router.js
@@ -5,7 +5,6 @@ import phoneRouter from './modules/phone/phone.router.js';
 import versionRouter from './modules/version/version.router.js';
 import cartRouter from './modules/cart/cart.router.js';
 import orderRouter from './modules/order/order.router.js';
-import reviewRouter from './modules/review/review.router.js';
 
 const initApp = async (app, express) => {
   app.use(express.json());
@@ -23,7 +22,6 @@ const initApp = async (app, express) => {
   app.use("/version", versionRouter);
   app.use("/cart", cartRouter);
   app.use("/order", orderRouter);
-  app.use("/review", reviewRouter);
 
   app.get("/", (req, res) => {
     return res.status(200).json({ message: "welcome ..." });
